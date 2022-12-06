3bfe6ac9d03fe3858817d05c5cc269fbe8ee1c72
Troy Liu <troyliu0105@outlook.com>
2022-12-06 09:32:58 +0800

    A: [Config] yolov6_s_syncbn_fast_8xb32-100e_chatphone_ng.py

--- mmyolo/datasets/__init__.py
+++ mmyolo/datasets/__init__.py
@@ -5,8 +5,9 @@ from .yolov5_coco import YOLOv5CocoDataset
 from .yolov5_voc import YOLOv5VOCDataset
 from .yolov5_autorois import YOLOv5AutoROISDataset
 from .yolov5_chatphone import YOLOv5ChatPhoneDataset
+from .yolov5_chatphone_ng import YOLOv5ChatPhoneNGDataset
 
 __all__ = [
     'YOLOv5CocoDataset', 'YOLOv5VOCDataset', 'BatchShapePolicy',
-    'yolov5_collate', 'YOLOv5AutoROISDataset', 'YOLOv5ChatPhoneDataset'
+    'yolov5_collate', 'YOLOv5AutoROISDataset', 'YOLOv5ChatPhoneDataset', 'YOLOv5ChatPhoneNGDataset'
 ]
---
+++ mmyolo/datasets/yolov5_chatphone_ng.py
@@ -0,0 +1,30 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from mmdet.datasets import XMLDataset
+
+from mmyolo.datasets.yolov5_coco import BatchShapePolicyDataset
+from ..registry import DATASETS
+
+
+@DATASETS.register_module()
+class ChatPhoneNGDataset(XMLDataset):
+    """Dataset for PASCAL VOC."""
+
+    METAINFO = {
+        'CLASSES':
+            ('phone', 'head_left', 'head_straight', 'head_right'),
+        # PALETTE is a list of color tuples, which is used for visualization.
+        'PALETTE': [(106, 0, 228), (119, 11, 32), (165, 42, 42)]
+    }
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+@DATASETS.register_module()
+class YOLOv5ChatPhoneNGDataset(BatchShapePolicyDataset, ChatPhoneNGDataset):
+    """Dataset for YOLOv5 VOC Dataset.
+
+    We only add `BatchShapePolicy` function compared with VOCDataset. See
+    `mmyolo/datasets/utils.py#BatchShapePolicy` for details
+    """
+    pass
